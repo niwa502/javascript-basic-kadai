7e60b3330b2898c155d8989bc910100ae48af6f9
niwa502 <152929961+niwa502@users.noreply.github.com>
2024-01-10 19:23:38 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+var dateName = new Date();
+
+var year = dateName.getFullYear();
+var month = dateName.getMonth() + 1;
+var day = dateName.getDate();
+
+console.log( year + '年' + month + '月' + day + '日');
