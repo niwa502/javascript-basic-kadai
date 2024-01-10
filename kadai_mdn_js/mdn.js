0a44f8cec35c6c486ea9ddfde42bde06a444c03a
niwa502 <152929961+niwa502@users.noreply.github.com>
2024-01-10 19:23:13 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-var dateName = new Date([2023,8,22]);
-
-var year = dateName.getFullYear();
-var month = dateName.getMonth() + 1;
-var day = dateName.getDate();
-
-console.log( year + '年' + month + '月' + day + '日');
